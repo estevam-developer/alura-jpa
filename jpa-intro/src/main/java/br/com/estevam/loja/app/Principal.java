4fbeefabe561867f93d0ddf12731e84be7fac089
Leonardo Alessandro Estevam <estevam.developer@gmail.com>
2022-05-17 14:54:18 -0300

    Criação do projeto com as configurações iniciais e com um primeiro exemplo de uso do JPA com Hibernate

---
+++ jpa-intro/src/main/java/br/com/estevam/loja/app/Principal.java
@@ -0,0 +1,33 @@
+package br.com.estevam.loja.app;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.estevam.loja.modelo.Produto;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		Produto celular = new Produto();
+		
+		celular.setNome("Xiaome Redmi Note 9");
+		celular.setDescicao("Smartphone Xiaomi Redmi Note 9 - 64GB/4GB Ram Versão Global");
+		celular.setPreco(new BigDecimal(1149));
+		
+		/**********************************************************************************/
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+		EntityManager em = factory.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(celular);
+		em.getTransaction().commit();
+		em.close();
+
+	}
+
+}
